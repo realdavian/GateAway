6cdc5580e49f097058b7bc07ba597720df5ea05e
Wei Xin <limweixin17@gmail.com>
2025-12-12 20:13:27 +0800

    Initial Commit

---
+++ TsukubaVPNGate/TsukubaVPNGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TsukubaVPNGateApp.swift
+//  TsukubaVPNGate
+//
+//  Created by Wei Xin on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TsukubaVPNGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
